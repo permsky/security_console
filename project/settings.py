186845bdbc3d3761609fdecb9bf4d5a545357fa8
Vitaly Borodulin <rainbowf@mail.ru>
2022-02-23 13:00:06 +0500

    Move secret settings in .env file

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,25 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=None)
 
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG')
 
 ROOT_URLCONF = "project.urls"
 
