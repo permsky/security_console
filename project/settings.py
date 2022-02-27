bfc8bc00ce30edebc33c119179dc9a10c14d2982
Vitaly Borodulin <rainbowf@mail.ru>
2022-02-27 15:28:06 +0500

    Fix ALLOWED_HOSTS setting

--- project/settings.py
+++ project/settings.py
@@ -19,7 +19,7 @@ DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
