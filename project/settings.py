e3b7fbdeaad85f22300a3ff7a82138a2a6333fdd
Vitaly Borodulin <rainbowf@mail.ru>
2022-02-27 13:52:53 +0500

    Use DATABASE_URL environment variable for database configuration

--- project/settings.py
+++ project/settings.py
@@ -1,19 +1,13 @@
 import os
+
+import dj_database_url
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 INSTALLED_APPS = ['datacenter']
 
